6862cda061ce1cd9ce927392318dbe1be3007dd0
deependu5960 <deependumis3@gmail.com>
2026-06-29 07:58:58 +0530

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import requests
 SERVER_URL = "https://crack-hex-server.vercel.app/"
 
 def check_database_cache(hash_value):
-    """Checks your Neon database via Vercel to see if the hash is already cracked."""
+    """Checks your Crack-Hex database via Server to see if the hash is already cracked."""
     try:
         response = requests.get(f"{SERVER_URL}/lookup", params={"hash_value": hash_value}, timeout=4)
         if response.status_code == 200:
@@ -12,11 +12,11 @@ def check_database_cache(hash_value):
             if data.get("found"):
                 return data  # Returns {"found": True, "plain_text": "...", "algo": "..."}
     except Exception as e:
-        print(f"[*] Database cache bypass (Server offline or timeout): {e}")
+        print(f"\033[1;91m[*] Database cache bypass (Server offline or timeout):\033[0m {e}")
     return {"found": False}
 
 def save_to_database_cache(hash_value, algo, plain_text):
-    """Sends a newly cracked hash to your Neon database so you never have to crack it again."""
+    """Sends a newly cracked hash to your Crack-Hex database so you never have to crack it again."""
     try:
         payload = {
             "hash_value": hash_value,
@@ -24,6 +24,6 @@ def save_to_database_cache(hash_value, algo, plain_text):
             "plain_text": plain_text
         }
         requests.post(f"{SERVER_URL}/store", json=payload, timeout=4)
-        print("[+] Successfully saved hash mapping to central Neon DB!")
+        print("[+] Successfully saved hash mapping to central Crack-Hex DB!")
     except Exception as e:
-        print(f"[*] Failed to save to database cache: {e}")
+        print(f"\033[1;91m[*] Failed to save to database cache:\033[0m {e}")
