386b0ab0b6d14cedce84412637818fbdcca31dcb
deependu5960 <deependumis3@gmail.com>
2026-06-29 08:11:16 +0530

    Fix formatting of instant match found message

--- start.py
+++ start.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         else:
             cache_result = check_database_cache(hash_pwd)
             if cache_result["found"]:
-                print(f"[+] Instant Match Found in Crack-Hex DB!")
+                print(f"\n[+] Instant Match Found in Crack-Hex DB!")
                 print(f"[+] Algorithm : {cache_result['algo']}")
                 print(f"\033[1;92m[+] Password : {cache_result['plain_text']}\033[0m")
                 # Exit early here because we already have the answer!
