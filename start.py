4c29402687377f18112322015c9740aaea6b7056
deependu5960 <deependumis3@gmail.com>
2026-06-29 08:23:17 +0530

    Remove newline print before hash input
    Removed unnecessary newline print statement.

--- start.py
+++ start.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
         print(f"\033[94m{banner}\033[0m")
         
         hash_pwd = input(f"\033[1;91m Enter Your Hash : \033[0m")
-        print("\n")
         if hash_pwd.lower() =="exit":
             exit()
         else:
