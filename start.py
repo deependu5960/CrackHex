5df87f1fe108ed3e82da367098813403f84c7873
deependu5960 <deependumis3@gmail.com>
2026-06-29 08:18:40 +0530

    Update start.py

--- start.py
+++ start.py
@@ -17,12 +17,13 @@ if __name__ == "__main__":
         print(f"\033[94m{banner}\033[0m")
         
         hash_pwd = input(f"\033[1;91m Enter Your Hash : \033[0m")
+        print("\n)
         if hash_pwd.lower() =="exit":
             exit()
         else:
             cache_result = check_database_cache(hash_pwd)
             if cache_result["found"]:
-                print(f"\n[+] Instant Match Found in Crack-Hex DB!")
+                print(f"[+] Instant Match Found in Crack-Hex DB!")
                 print(f"[+] Algorithm : {cache_result['algo']}")
                 print(f"\033[1;92m[+] Password : {cache_result['plain_text']}\033[0m")
                 # Exit early here because we already have the answer!
